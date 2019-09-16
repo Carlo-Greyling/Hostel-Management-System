02658c5656678e1b496dea6e377ab99ec65c49d5
Keanu Jooste <keanujooste000@gmail.com>
2019-09-16 16:36:02 +0000

    Login form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginSignupComponent } from './login-signup/login-signup.component';
+import {MatButtonModule, MatCardModule, MatFormFieldModule, MatInputModule} from '@angular/material';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,13 @@ import { LoginSignupComponent } from './login-signup/login-signup.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCardModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatButtonModule,
+    MatInputModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login-signup/login-signup.component.ts
+++ src/app/login-signup/login-signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-login-signup',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-signup.component.scss']
 })
 export class LoginSignupComponent implements OnInit {
+  signInMessage = 'Please Log In'
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  LogIn(form: NgForm) {
+    const value = form.value;
+    if (value.username === 'admin') {
+      if (value.password === 'admin') {
+        // this.router.navigate(['homeComponent']);
+      }
+    }
+  }
 }
