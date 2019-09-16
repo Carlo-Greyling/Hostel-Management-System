63f0f71196765b2b7b24f9dd9e8c2fd743ce1960
Keanu Jooste <keanujooste000@gmail.com>
2019-09-16 16:03:40 +0000

    App routing and custom themme

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginSignupComponent } from './login-signup/login-signup.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
+  { path: 'login', component: LoginSignupComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
