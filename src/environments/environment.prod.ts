f606963fa67e93dc131f10e7e32b0854eb1741e5
Keanu Jooste <keanujooste000@gmail.com>
2019-09-23 07:51:40 +0000

    Login hotfix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCVnzKfAtDqqzo5z6J59sjwFy3gXwWDtfI',
+    authDomain: 'itrw324-27aad.firebaseapp.com',
+    databaseURL: 'https://itrw324-27aad.firebaseio.com',
+    projectId: 'itrw324-27aad',
+    storageBucket: 'itrw324-27aad.appspot.com',
+    messagingSenderId: '854632301243',
+    appId: '1:854632301243:web:32bf7bae3f7d0009199f9e'}
 };
