cefcd877fdba8d0531c505de8983034570dd1086
carlosilveraxe <carlosilveraxe@gmail.com>
2019-09-30 23:41:22 +0200

    Added user profile

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
+  studentNumber = localStorage.getItem('email').substring(0, 7);
+  studentName = localStorage.getItem('username');
+  studentEmail = localStorage.getItem('email');
 
   constructor() { }
 
