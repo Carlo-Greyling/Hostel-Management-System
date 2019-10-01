c1afe84136f6882d5b0ba085bc1e828ae64ffefe
Keanu Jooste <keanujooste000@gmail.com>
2019-10-01 08:48:26 +0000

    Calendar works with firebase now

--- src/app/calender/calender.component.ts
+++ src/app/calender/calender.component.ts
@@ -5,6 +5,7 @@ import interactionPlugin from '@fullcalendar/interaction';
 import {CalendarEvent} from '../models/calendarevent.model';
 import {MatDialog} from "@angular/material/dialog";
 import {EventDetailsComponent} from "../event-details/event-details.component";
+import {FirebaseService} from "../services/firebase.service";
 
 /*export interface CalendarEvent {
   title: string;
@@ -19,17 +20,19 @@ import {EventDetailsComponent} from "../event-details/event-details.component";
 })
 export class CalenderComponent implements OnInit {
   calendarPlugins = [dayGridPlugin, interactionPlugin];
-  events: CalendarEvent[] = [
+  events: CalendarEvent[] = [];
+  /*events: CalendarEvent[] = [
     {title: 'Event 1', date: '2019-09-24', description: 'event 1'},
     {title: 'Event 2', date: '2019-09-25', description: 'event 2'},
     {title: 'Event 3', date: '2019-09-26', description: 'event 3'},
     {title: 'Event 4', date: '2019-09-27', description: 'event 4'},
     {title: 'Event 5', date: '2019-09-28', description: 'event 5'}
-  ];
+  ];*/
 
-  constructor(private dialog: MatDialog) { }
+  constructor(private dialog: MatDialog, private fbs: FirebaseService) {}
 
   ngOnInit() {
+    this.events = this.fbs.getEvents();
   }
 
   /*SubmitAddFields() {
