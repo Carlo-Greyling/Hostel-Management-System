4472398cb0f4a96a43e9b7fcf6c2907942aaf6cc
carlosilveraxe <carlosilveraxe@gmail.com>
2019-10-25 10:03:14 +0200

    Repairs list displays repairs

--- src/app/repairs-list/repairs-list.component.ts
+++ src/app/repairs-list/repairs-list.component.ts
@@ -7,16 +7,29 @@ import { Ticket } from '../services/ticket.model';
   styleUrls: ['./repairs-list.component.scss']
 })
 export class RepairsListComponent implements OnInit {
-  ticketNumber: string;
+  ticketNumber;
   hostelID = 'Hombre';
   roomNumber;
   studentNumber;
   studentName;
   studentEmail;
   description;
+  y = 0;
 
-  repairsDetails: string[] = [
-    'New Repairs Ticket ' + 'UUP4QC7S' + ' Generated, Details As Follows: ' +
+  repairsTickets = [
+    {ticketNumber : 'UUP4QC7S',
+      ticketEmail: 'New Repairs Ticket '  + 'UUP4QC7S' + ' Generated, Details As Follows: ' +
+    'Hostel: ' + this.hostelID +
+    'Room Number: ' + '12' +
+
+    'Student Number: ' + '29685532' +
+    'Student Name: ' + 'Carlo Greyling' +
+    'Student Email: ' + '29685532@student.g.nwu.ac.za'
+        +
+    ' Description: ' + 'Broken Window'},
+
+    {ticketNumber : '4FYFM04Y',
+      ticketEmail: 'New Repairs Ticket ' + 'UUP4QC7S' + ' Generated, Details As Follows: ' +
     'Hostel: ' + this.hostelID +
     'Room Number: ' + '12' +
 
@@ -24,17 +37,49 @@ export class RepairsListComponent implements OnInit {
     'Student Name: ' + 'Carlo Greyling' +
     'Student Email: ' + '29685532@student.g.nwu.ac.za' +
 
-    'Description: ' + 'Broken Window',
+    ' Description: ' + 'Broken Window'},
+
+    {ticketNumber : 'LIWQVPWA',
+      ticketEmail: 'New Repairs Ticket ' + 'UUP4QC7S' + ' Generated, Details As Follows: ' +
+    'Hostel: ' + this.hostelID +
+    'Room Number: ' + '12' +
+
+    'Student Number: ' + '29685532' +
+    'Student Name: ' + 'Carlo Greyling' +
+    'Student Email: ' + '29685532@student.g.nwu.ac.za' +
+
+    ' Description: ' + 'Broken Window'},
+
+    {ticketNumber : 'Y8B6B0NM',
+      ticketEmail: 'New Repairs Ticket ' + 'UUP4QC7S' + ' Generated, Details As Follows: ' +
+    'Hostel: ' + this.hostelID +
+    'Room Number: ' + '12' +
+
+    'Student Number: ' + '29685532' +
+    'Student Name: ' + 'Carlo Greyling' +
+    'Student Email: ' + '29685532@student.g.nwu.ac.za' +
+
+    ' Description: ' + 'Broken Window'},
+
+    {ticketNumber : '1WQ5DO7Q',
+      ticketEmail: 'New Repairs Ticket ' + 'UUP4QC7S' + ' Generated, Details As Follows: ' +
+    'Hostel: ' + this.hostelID +
+    'Room Number: ' + '12' +
+
+    'Student Number: ' + '29685532' +
+    'Student Name: ' + 'Carlo Greyling' +
+    'Student Email: ' + '29685532@student.g.nwu.ac.za' +
+
+    ' Description: ' + 'Broken Window'},
   ];
 
-  repairsTickets: string[] = [
-    'UUP4QC7S',
-    '4FYFM04Y',
-    'LIWQVPWA',
-    'Y8B6B0NM',
-    '1WQ5DO7Q',
-  ];
-  i;
+  /*repairsTickets = [
+    {ticketNumber : 'UUP4QC7S'},
+    {ticketNumber : '4FYFM04Y'},
+    {ticketNumber : 'LIWQVPWA'},
+    {ticketNumber : 'Y8B6B0NM'},
+    {ticketNumber : '1WQ5DO7Q'}
+  ];*/
 
   populateArray() {
     /*for (let i = 0; i < 5; i++) {
